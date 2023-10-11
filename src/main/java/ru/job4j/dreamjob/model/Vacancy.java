c98d2a05a8e59502f483fa0ca7aa32feefdf2485
GrnDev1 <grndev@mail.ru>
2023-10-11 16:47:22 +0300

    2. Формы. Списки. [#504854]

--- src/main/java/ru/job4j/dreamjob/model/Vacancy.java
+++ src/main/java/ru/job4j/dreamjob/model/Vacancy.java
@@ -9,6 +9,7 @@ public class Vacancy {
     private String description;
     private LocalDateTime creationDate = LocalDateTime.now();
     private boolean visible;
+    private int cityId;
 
     public Vacancy() {
     }
@@ -19,11 +20,20 @@ public class Vacancy {
         this.description = description;
     }
 
-    public Vacancy(int id, String title, String description, LocalDateTime creationDate, boolean visible) {
+    public Vacancy(int id, String title, String description, LocalDateTime creationDate, boolean visible, int cityId) {
         this.id = id;
         this.title = title;
         this.description = description;
         this.visible = visible;
+        this.cityId = cityId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
     }
 
     public boolean getVisible() {
